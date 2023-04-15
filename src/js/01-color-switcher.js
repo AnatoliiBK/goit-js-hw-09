5836aed61a0eadb66aec70a8cb7cdbb1de8a1f03
AnetRo <beatrice.ccq@gmail.com>
2023-04-15 15:26:43 +0300

    01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,52 +2,30 @@ const bodyColor = document.querySelector("body");
 const startBtn = document.querySelector("[data-start]");
 const stopBtn = document.querySelector("[data-stop]");
 let colorRan = null;
-console.log(bodyColor)
-console.log(startBtn)
-console.log(stopBtn)
+
+startBtn.addEventListener("click", bgdBody);
+stopBtn.setAttribute("disabled", true);
 
 
-
-startBtn.addEventListener("click", colorRan)
-
-bodyColor.style.backgroundColor = getRandomHexColor();
-console.log(getRandomHexColor())
 
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-  }
-  colorRan = setInterval(getRandomHexColor, 1000)
-  console.log(colorRan)
+}
 
-  stopBtn.addEventListener("click", () => {
-    clearInterval(colorRan);
-    console.log(`Interval with id ${timerId} has stopped!`);
-  });
-
-
-
-
-// function getRandomHexColor() {
-//     timerId = setInterval(() => {`#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`}, 1000);
-//     return timerId;
-//   }
+function bgdBody() {
+  startBtn.setAttribute("disabled", true);
+  stopBtn.removeAttribute("disabled", true);
+  startBtn.removeEventListener("click", bgdBody);
+  colorRan = setInterval(() => {
+    bodyColor.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+} 
 
 
-  // startBtn.addEventListener("click", () => {
-//     // bgdColor.style.backgroundColor = setInterval(() => {`#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`}, 1000);
-    
-//     bodyColor.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-//     let colorBgd = bodyColor.style.backgroundColor;
-//     console.log(colorBgd)
-//     colorBgd = setInterval(() => {
-//         `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`, 1000
-//     })
-// })
-   
-
-
-// startBtn.addEventListener("click", () => {
-//     timerId = setInterval(() => {
-//       console.log(`I love async JS!  ${Math.random()}`);
-//     }, 1000);
-//   });
+stopBtn.addEventListener("click", () => {
+  startBtn.removeAttribute("disabled", true)
+  startBtn.addEventListener("click", bgdBody)
+  stopBtn.setAttribute("disabled", true);
+  clearInterval(colorRan);
+  console.log(`Interval with id ${colorRan} has stopped!`);
+});
