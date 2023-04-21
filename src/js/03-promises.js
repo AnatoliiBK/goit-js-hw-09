531245a3aee7db72ffee1adf281a312807fd6f38
AnetRo <beatrice.ccq@gmail.com>
2023-04-21 13:06:54 +0300

    fix promice & func

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,64 +1,47 @@
 const formR = document.querySelector(".form");
-const inputDel = document.querySelector("[name=delay]");
-const inputStep = document.querySelector("[name=step]");
-const inputAm = document.querySelector("[name=amount]");
-const btnForm = document.querySelector("button")
-let timerId = 0;
  
-formR.addEventListener("submit", createPromise);
+formR.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventdefault();
+  
+  const {delay, step, amount} = event.target.elements;
+  
+  const delayValue = Number(delay.value);
+  const amountValue = Number(amount.value);
+  const stepValue = Number(step.value);
+  
+
+  for (let i = 1; i <= amountValue; i += 1) {
+    createPromise(i, delayValue).then(({position, delay}) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({position, delay}) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+    delayValue += stepValue;
+  }
+  
+}
 
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  const success = {};
-  const notsuccess = {};
-    if (shouldResolve) {
-    // Fulfill
-      for (let i = 1; i <= position; i += 1) {     
-          position = i;
-      }
-      for (let j = inputDel.value; j >= inputDel.value; j += inputStep) {
-        delay = j;     
-      }
-      
-      return success = {
-        position: i,
-        delay: j
-      }
-
-        } else {
-          // Reject
-          for (let i = 1; i <= position; i += 1) {     
-            position = i;
-        }
-          for (let j = inputDel.value; j >= inputDel.value; j += inputStep) {
-            delay = j;
-          }
-
-        return notsuccess = {
-          position: i,
-          delay: j
-        }
-        }
-      }
-
-  
-  const promice = new Promice((res, rej) => {
+  return promice = new Promice((res, rej) => {
     setInterval(() => {
-      if (success) {
-        res(console.log(`✅ Fulfilled promise ${position} in ${delay}ms`))
+      if (shouldResolve) {
+        res({position, delay})
       }
-      rej(console.log(`❌ Rejected promise ${position} in ${delay}ms`))
+      rej({position, delay})
     })
   }, delay)
+  }
+
+  
+  
 
 
-  promice.then((success) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch((notsuccess) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
+  
 
 
 
@@ -77,3 +60,35 @@ function createPromise(position, delay) {
 //   .catch(({ position, delay }) => {
 //     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
 //   });
+
+
+// const success = {};
+//   const notsuccess = {};
+//     if (shouldResolve) {
+//     // Fulfill
+//       for (let i = 1; i <= position; i += 1) {     
+//           position = i;
+//       }
+//       for (let j = inputDel.value; j >= inputDel.value; j += inputStep) {
+//         delay = j;     
+//       }
+      
+//       return success = {
+//         position: i,
+//         delay: j
+//       }
+
+//         } else {
+//           // Reject
+//           for (let i = 1; i <= position; i += 1) {     
+//             position = i;
+//         }
+//           for (let j = inputDel.value; j >= inputDel.value; j += inputStep) {
+//             delay = j;
+//           }
+
+//         return notsuccess = {
+//           position: i,
+//           delay: j
+//         }
+//         }
